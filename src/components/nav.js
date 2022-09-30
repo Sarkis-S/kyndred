bcf3e16d8fa9ef12806c1594c42543c98a84a39c
Sarkis-S <Sarkis.Sos19@gmail.com>
2022-09-30 10:42:46 -0700

    Build Home page

---
+++ src/components/nav.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import HomeIcon from '@mui/icons-material/Home';
+import GroupIcon from '@mui/icons-material/Group';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+
+export default function NavIconLabelTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    console.log('Show events:', event);
+    setValue(newValue);
+  };
+
+  return (
+    <Tabs value={value} onChange={handleChange} aria-label="navigation icon label tabs">
+      <Tab icon={<HomeIcon />} label="HOME" />
+      <Tab icon={<GroupIcon />} label="EMPLOYEES" />
+      <Tab icon={<GroupAddIcon />} label="ONBOARDING" />
+    </Tabs>
+  );
+}
